a151a99dede280f9b5aa48b6d5ccf6cc46850760
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 20:43:52 +0100

    display the array texts in the yellow-text function

--- src/components/Yellow-text.tsx
+++ src/components/Yellow-text.tsx
@@ -1,8 +1,16 @@
+import { tickText } from "@/utilities/yellow-text";
+import { works } from "@/utilities/yellow-text";
+import { benefits } from "@/utilities/yellow-text";
+
 export const YellowText = () => {
     return (
         <div>
-            <span className="yellow-bg">1</span>
-            <span className="yellow-text">Introduction to the course</span>
+            {tickText.map((item: { id: number; text: string }, id: number) => (
+                <div key={id}>
+                    <span className="yellow-bg">{item.id}</span>
+                    <span className="yellow-text">{item.text}</span>
+                </div>
+            ))}
         </div>
     );
 };
