25ea62db2225324a0721dc4130cda26a0ed107d1
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 20:24:51 +0100

    create arrays to store the text for the yellow-text component

---
+++ src/utilities/yellow-text.ts
@@ -0,0 +1,18 @@
+export const tickText = [
+    { id: 1, text: "Average return on investment of 7%" },
+    { id: 2, text: "Withdraw your funds every 3 months" },
+    { id: 3, text: "Investment selection from qualified SGRs" },
+];
+
+export const works = [
+    { id: 1, text: "Create your profile in 3 minutes" },
+    { id: 2, text: "Invest in our real estate portfolio" },
+    { id: 3, text: "View the trend of collected rents and appreciation" },
+];
+
+export const benefits = [
+    { id: 1, text: "Start without the need for large sums" },
+    { id: 2, text: "Forget about operational management" },
+    { id: 3, text: "Get liquidity in 3 months" },
+    { id: 4, text: "Benefit from diversification" },
+];
