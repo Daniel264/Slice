cae95fcc4d27a77fa7d0b27307035200ad456d61
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 21:26:48 +0100

    Refactor YellowText component to accept dynamic text array as props

--- src/components/Yellow-text.tsx
+++ src/components/Yellow-text.tsx
@@ -2,10 +2,14 @@ import { tickText } from "@/utilities/yellow-text";
 import { works } from "@/utilities/yellow-text";
 import { benefits } from "@/utilities/yellow-text";
 
-export const YellowText = () => {
+interface TextProps {
+    array: { id: number; text: string }[];
+}
+
+export const YellowText = ({array}: TextProps) => {
     return (
         <div className="yellow-container">
-            {tickText.map((item: { id: number; text: string }, id: number) => (
+            {array.map((item: { id: number; text: string }, id: number) => (
                 <div key={id}>
                     <span className="yellow-bg">{item.id}</span>
                     <span className="yellow-text">{item.text}</span>
