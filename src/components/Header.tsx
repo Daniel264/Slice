afc132de91ae79465e3e448b21e92855250fbc46
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 19:15:12 +0100

    my code to use @forward and @use well

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,23 @@ import { Head } from "@/utilities/header";
 
 const Header = () => {
     return (
-        <div className="flex h-fit w-full justify-between px-4 py-5 header-div">
-            <div className="flex cursor-pointer gap-[2px] header-logo">
-                <h1 className="text-3xl font-semibold logo-text">Splice</h1>
+        <div className=" header-div">
+            <div className=" header-logo">
+                <h1 className=" logo-text">Splice</h1>
                 <Image src={"/assets/images/logo.svg"} alt={""} width={50} height={50} />
             </div>
             <div className="header-middle">
-                <ul className="flex cursor-pointer space-x-3 text-lg font-medium md:space-x-6 lg:text-xl ">
+                <ul>
                     {Head.map((item, index) => (
                         <div key={index}>
-                            <li className="hover:text-[#888]">{item.name}</li>
+                            <li>{item.name}</li>
                         </div>
                     ))}
                 </ul>
             </div>
             <div className="header-end">
                 <p>Contact us</p>
-                <button className="rounded-lg bg-[#1A1A1A] px-4 py-2 text-white">Get this template</button>
+                <button>Get this template</button>
             </div>
         </div>
     );
