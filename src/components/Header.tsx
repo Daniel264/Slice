a7e15b7b01ae682faa5078a6c2d0df04ca91a33b
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 18:57:27 +0100

    Convert all header tailwind styles to scss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import { Head } from "@/utilities/header";
 
 const Header = () => {
     return (
-        <div className="flex h-fit w-full justify-between px-4 py-5">
-            <div className="flex cursor-pointer gap-[2px]">
-                <h1 className="text-3xl font-semibold">Splice</h1>
+        <div className="flex h-fit w-full justify-between px-4 py-5 header-div">
+            <div className="flex cursor-pointer gap-[2px] header-logo">
+                <h1 className="text-3xl font-semibold logo-text">Splice</h1>
                 <Image src={"/assets/images/logo.svg"} alt={""} width={50} height={50} />
             </div>
-            <div>
+            <div className="header-middle">
                 <ul className="flex cursor-pointer space-x-3 text-lg font-medium md:space-x-6 lg:text-xl ">
                     {Head.map((item, index) => (
                         <div key={index}>
@@ -18,7 +18,7 @@ const Header = () => {
                     ))}
                 </ul>
             </div>
-            <div>
+            <div className="header-end">
                 <p>Contact us</p>
                 <button className="rounded-lg bg-[#1A1A1A] px-4 py-2 text-white">Get this template</button>
             </div>
