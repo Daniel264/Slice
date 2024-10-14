681f3a9674d0dc901b55812d232bbf9cf124b4f3
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 16:58:11 +0100

    change text-color or hover and pointer type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,15 @@ import { Head } from "@/utilities/header";
 const Header = () => {
     return (
         <div className="flex h-fit w-full justify-between px-4 py-5">
-            <div className="flex gap-[2px]">
+            <div className="flex cursor-pointer gap-[2px]">
                 <h1 className="text-3xl font-semibold">Splice</h1>
                 <Image src={"/assets/images/logo.svg"} alt={""} width={50} height={50} />
             </div>
             <div>
-                <ul className="flex space-x-3 text-lg font-medium">
+                <ul className="flex cursor-pointer space-x-3 text-lg font-medium ">
                     {Head.map((item, index) => (
                         <div key={index}>
-                            <li>{item.name}</li>
+                            <li className="hover:text-[#888]">{item.name}</li>
                         </div>
                     ))}
                 </ul>
