de038663ff225f80f458d2a534bf7cc899fa6191
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 20:13:13 +0100

    Add additional styles to the yellow-text component

--- src/components/Yellow-text.tsx
+++ src/components/Yellow-text.tsx
@@ -2,6 +2,7 @@ export const YellowText = () => {
     return (
         <div>
             <span className="yellow-bg">1</span>
+            <span className="yellow-text">Introduction to the course</span>
         </div>
     );
 };
