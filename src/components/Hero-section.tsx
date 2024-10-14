e4e4751d722059adde5548acb1b5dc3e4e967531
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 19:58:24 +0100

    define the structure for the hero-section component

---
+++ src/components/Hero-section.tsx
@@ -0,0 +1,18 @@
+export const HeroSection = () => {
+    return (
+        <section>
+            <div>
+                <h1>
+                    Get Real Estate <br />
+                    income, without <br />
+                    owning a house
+                </h1>
+
+                <div>
+                   
+                </div>
+            </div>
+            <div></div>
+        </section>
+    );
+};
