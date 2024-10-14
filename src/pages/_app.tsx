11c59b260ea53b83eca0ac44e2b50504eeefe185
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-14 18:37:56 +0100

    Generate a scss file and generate a css file from that

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "@/styles/globals.css";
+import "@/styles/SCSS/index.css";
 import { ToastContainer } from "react-toastify";
 import { NextRouter, useRouter } from "next/router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
