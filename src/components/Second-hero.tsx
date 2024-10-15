a2d53914c6b9ab1cd4ff5a40c43e66f2aa6406b8
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-15 22:52:28 +0100

    feat: add a straight image and align it with the second-hero

--- src/components/Second-hero.tsx
+++ src/components/Second-hero.tsx
@@ -2,16 +2,20 @@
 import { works } from "@/utilities/yellow-text";
 import { HeroSection } from "./Hero-section-Template";
 import { Badge } from "./Badge";
+import Image from "next/image";
 
 export const SecondHero = () => {
     return (
-        <section>
+        <section className="second-hero">
             <div className="badge-div">
                 <Badge>How does it work</Badge>
             </div>
-            <h1>
-                Invest in income-generating <br /> real estate easily
-            </h1>
+            <div>
+                <Image className="straight-image" src={"/assets/images/Second-hero-img.svg"} alt={""} height={90} width={300}/>
+                <h1>
+                    Invest in income-generating <br /> real estate easily
+                </h1>
+            </div>
             <HeroSection flexDirection={"row-reverse"} designedText={false} src={"/assets/images/section-img.svg"} arrayName={works} />
         </section>
     );
