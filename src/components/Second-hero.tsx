478a71aa014fb1c3c5552ca05b2101a61b1fb427
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-15 21:12:57 +0100

    Add a header to the second-hero

--- src/components/Second-hero.tsx
+++ src/components/Second-hero.tsx
@@ -4,6 +4,9 @@ import { HeroSection } from "./Hero-section-Template";
 export const SecondHero = () => {
     return (
         <section>
+            <h1>
+                Invest in income-generating <br /> real estate easily
+            </h1>
             <HeroSection flexDirection={"row-reverse"} designedText={false} src={"/assets/images/section-img.svg"} />
         </section>
     );
