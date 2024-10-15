92f55cf30fa7867c83dac0688a2837ca853d6f18
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-15 13:46:39 +0100

    add CompaniesCarousel component and update header styles

---
+++ src/components/CompaniesCarousel.tsx
@@ -0,0 +1,9 @@
+
+
+export const CompaniesCarousel = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/utilities/carousel.ts
@@ -0,0 +1,50 @@
+export const FisrtCarousel = () => [
+    {
+        id: 1,
+        title: "Facebook",
+        image: "/assets/images/facebook.svg",
+    },
+    {
+        id: 2,
+        title: "Ideo",
+        image: "/assets/images/ideo.svg",
+    },
+    {
+        id: 3,
+        title: "Notion",
+        image: "/assets/images/notion.svg",
+    },
+    {
+        id: 3,
+        title: "Zapier",
+        image: "/assets/images/zapier.svg",
+    },
+    {
+        id: 3,
+        title: "Gum",
+        image: "/assets/images/gum.svg",
+    },
+];
+
+export const SecondCarousel = () => [
+    {
+        id: 1,
+        title: "caa",
+        image: "/assets/images/caa.svg",
+    },
+    {
+        id: 2,
+        title: "coinbase",
+        image: "/assets/images/coinbase.svg",
+    },
+    {
+        id: 3,
+        title: "tq",
+        image: "/assets/images/tq.svg",
+    },
+    {
+        id: 3,
+        title: "sequoia",
+        image: "/assets/images/sequoia.svg",
+    },
+];
