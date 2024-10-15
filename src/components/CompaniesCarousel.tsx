e258d26ff72a00421f56179af945b15cc61fdafd
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-15 13:50:18 +0100

    add CompaniesCarousel component with carousel functionality and fix typo in carousel utility

--- src/components/CompaniesCarousel.tsx
+++ src/components/CompaniesCarousel.tsx
@@ -1,9 +1,25 @@
-
+import { FirstCarousel, SecondCarousel } from "@/utilities/carousel";
+import { Key } from "react";
 
 export const CompaniesCarousel = () => {
     return (
         <div>
-            
+            <h3>Helping teams at the world&apos;s best companies</h3>
+
+            <div>
+                {FirstCarousel().map((company: { image: string | undefined; title: string | undefined }, index: Key | null | undefined) => (
+                    <div key={index}>
+                        <img src={company.image} alt={company.title} />
+                    </div>
+                ))}
+            </div>
+            <div>
+                {SecondCarousel().map((company: { image: string | undefined; title: string | undefined }, index: Key | null | undefined) => (
+                    <div key={index}>
+                        <img src={company.image} alt={company.title} />
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
--- src/utilities/carousel.ts
+++ src/utilities/carousel.ts
@@ -1,4 +1,4 @@
-export const FisrtCarousel = () => [
+export const FirstCarousel = () => [
     {
         id: 1,
         title: "Facebook",
