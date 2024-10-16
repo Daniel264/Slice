76357577d9178466080d6be0938ef64d7c4cbb66
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-16 08:57:18 +0100

    make the logo-carousel smooth

--- src/components/CompaniesCarousel.tsx
+++ src/components/CompaniesCarousel.tsx
@@ -2,9 +2,9 @@ import { FirstCarousel, SecondCarousel } from "@/utilities/carousel";
 import { Key, useMemo } from "react";
 
 export const CompaniesCarousel = () => {
-    const firstDuplicatedArray = useMemo(() => [...FirstCarousel(), ...FirstCarousel(), ...FirstCarousel()], []);
+    const firstDuplicatedArray = useMemo(() => [...FirstCarousel(), ...FirstCarousel(), ...FirstCarousel(), ...FirstCarousel()], []);
 
-    const secondDuplicatedArray = [...SecondCarousel(), ...SecondCarousel(), ...SecondCarousel()];
+    const secondDuplicatedArray = [...SecondCarousel(), ...SecondCarousel(), ...SecondCarousel(), ...SecondCarousel()];
     return (
         <div className="companies-description">
             <h3>Helping teams at the world&apos;s best companies</h3>
