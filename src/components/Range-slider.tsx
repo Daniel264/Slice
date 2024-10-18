d4cefeb9131893f4163a2ec052ad341f9d0c0c5f
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-18 10:38:49 +0100

    Remove all borders and outline from the styled input

--- src/components/Range-slider.tsx
+++ src/components/Range-slider.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import styles from 'styled-components';
+import styles from "styled-components";
 
-const RangeSlider:React.FC = () => {
-
+const RangeSlider: React.FC = () => {
     const SliderContainer = styles.div`
         display: flex;
         flex-direction: column;
@@ -12,14 +11,21 @@ const RangeSlider:React.FC = () => {
 
     const StyledInput = styles.input`
         margin: 10px 0;
+        border: none;
+        outline:none;
         width: 80%;
+        font-size: 4.5rem;
+        text-align: center;
+        color: #555;
+        font-weight: bold;
     `;
 
     const StyledRange = styles.input`
         width: 80%;
         -webkit-appearance: none;
         appearance: none;
-        height: 5px;
+        height: 10px;
+        border-radius: 1rem;
         background: #ddd;
         outline: none;
         opacity: 0.7;
@@ -35,6 +41,7 @@ const RangeSlider:React.FC = () => {
             width: 25px;
             height: 25px;
             background: #4CAF50;
+             border-radius: 1rem;
             cursor: pointer;
         }
 
@@ -48,16 +55,15 @@ const RangeSlider:React.FC = () => {
 
     const [value, setValue] = useState<number>(50);
 
-     const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setValue(Number(event.target.value));
-     }
+    };
 
     return (
         <SliderContainer>
             {/* <label htmlFor="range">${value}</label> */}
-            <StyledInput type="number" value={value} onChange={handleChange}/>
+            <StyledInput type="number" value={value} placeholder={"$"} onChange={handleChange} />
             <StyledRange type="range" min={0} max={100} value={value} onChange={handleChange} className="range-slider rating" />
-
         </SliderContainer>
     );
 };
