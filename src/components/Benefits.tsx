c0d37b4397e3da4558c2dc80b9542e5f31c964cd
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-18 23:19:37 +0100

    create and add the elements in the third section (benefits)

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -1,13 +1,19 @@
+import { works } from "@/utilities/yellow-text";
 import { Badge } from "./Badge"
+import { HeroSection } from "./Hero-section-Template";
 
 export const Benefits = () => {
     return (
-        <section>
+        <section className="badge-component">
             <div>
                 <Badge>Benefits</Badge>
                 <h1>
                     More Accessible and secure <br /> than going solo
                 </h1>
+
+                <div>
+                    <HeroSection flexDirection={"row"} designedText={false} src={"/assets/images/third-section.svg"} arrayName={works} />
+                </div>
             </div>
         </section>
     );
