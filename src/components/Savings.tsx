18979ab16bbf36fe6f7d38ca20db5051f66ac990
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-19 10:30:26 +0100

    create a savings component

---
+++ src/components/Savings.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Savings = () => {
+    return (
+        <section>
+            <article>
+                <h1>Optimise your return rate and retain more of your income</h1>
+
+                <p>
+                    Secure premium profits. We seamlessly provide you <br /> with the highest returns. Keep up to an extra 17.6% after taxes. some of your earnings could be exempt from local and city taxes.
+                </p>
+            </article>
+
+            <article>
+                
+            </article>
+        </section>
+    );
+};
+
+export default Savings;
