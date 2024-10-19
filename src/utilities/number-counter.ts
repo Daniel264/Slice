fd5c81092b3272d805e6d5710e464d9d164c8ae6
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-19 17:05:54 +0100

    Create an array for the number counter elements

---
+++ src/utilities/number-counter.ts
@@ -0,0 +1,5 @@
+export const NumberCounter = [
+    { id: 1, number: 11.7, sign: "x", text: "Attain prime returns. We effortlessly secure the utmost proceeds possible for you on here." },
+    { id: 2, number: 17.6, sign: "%", text: "Retain Up To 17.6%, More, Post-Tax. A portion of your income may be free from state and local taxes." },
+    { id: 3, number: 0.45, sign: "%", text: "Minor fee. We shift your funds when a superior rate emerges from our Premium feature set." },
+];
