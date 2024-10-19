d256085cbc06915750f2d83b49683ea52387b6b1
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-19 15:10:31 +0100

    align the SavingsWrapper element horizontally

--- src/components/Savings.tsx
+++ src/components/Savings.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 const StyledSpan = styled.span`
     color: #737373;
+    display: block;
+    text-align: left;
 `;
 
 const SavingsWrapper = styled.section`
@@ -23,14 +25,14 @@ const SavingsWrapper = styled.section`
 
 const Savings = () => {
     return (
-        <SavingsWrapper>
+        <SavingsWrapper className="savings-wrapper">
             <article>
                 <h1>
                     <StyledSpan>Optimise your return rate</StyledSpan> and retain more of your income
                 </h1>
 
                 <p>
-                    Secure premium profits. We seamlessly provide you <br /> with the highest returns. Keep up to an extra 17.6% after taxes. some of your earnings could be exempt from local and city taxes.
+                    Secure premium profits. We seamlessly provide you <br /> with the highest returns. Keep up to an extra <br /> 17.6% after taxes. some of your earnings could <br /> be exempt from local and city taxes.
                 </p>
             </article>
 
