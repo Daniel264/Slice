44d40d16c35f7263cb591a8be614838fb37455c4
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-19 09:50:45 +0100

    create an images array for the src of the card-images

---
+++ src/utilities/benefits-card.ts
@@ -0,0 +1,6 @@
+export const images = [
+    { id: 1, src: "/assets/images/card-one.svg" },
+    { id: 2, src: "/assets/images/card-two.svg" },
+    { id: 3, src: "/assets/images/card-three.svg" },
+    { id: 4, src: "/assets/images/card-four.svg" },
+];
