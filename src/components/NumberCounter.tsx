70b1490bcee7ac489863cc54c4920277a663ee1d
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-19 17:13:14 +0100

    Display the content by the passing the NumberCounterProps

--- src/components/NumberCounter.tsx
+++ src/components/NumberCounter.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { numbercounter } from "@/utilities/number-counter";
 
-const NumberCounter = () => {
-    return <div>
-        
-    </div>;
+interface NumberCounterProps {
+    id: number;
+    number: number;
+    sign: string;
+    text: string;
+}
+
+const NumberCounter = ({ id, number, sign, text }: NumberCounterProps) => {
+    return (
+        <div>
+            {numbercounter.map((item) => (
+                <div key={item.id}>
+                    <p>{item.number} + {item.text}</p>
+                    <p>{item.text}</p>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default NumberCounter;
