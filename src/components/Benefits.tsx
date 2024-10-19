a8ee169dd8f1d4454dc84461ebe22f8b758759fd
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-19 10:07:07 +0100

    display the cards in the benefits component

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -1,6 +1,8 @@
 import { benefits, works } from "@/utilities/yellow-text";
 import { Badge } from "./Badge"
 import { HeroSection } from "./Hero-section-Template";
+import BenefitsCards from "./BenefitsCards";
+import { images } from "@/utilities/benefits-card";
 
 export const Benefits = () => {
     return (
@@ -15,6 +17,7 @@ export const Benefits = () => {
                     <HeroSection flexDirection={"row"} designedText={false} src={"/assets/images/third-section.svg"} arrayName={benefits} />
                 </div>
             </div>
+            <BenefitsCards images={images} />
         </section>
     );
 };
