bc624a80554cc18c5ca7716b22b75e0a3034c491
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-20 15:50:42 +0100

    Fix ts type checking errors in the intersection observer

--- src/components/IntersectionObserver.tsx
+++ src/components/IntersectionObserver.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const IntersectionObserver = (options) => {
+const IntersectionObserver = (options: IntersectionObserverInit): [React.RefObject<HTMLElement>, boolean] => {
     const [isIntersecting, setIntersecting] = useState<boolean>(false);
-    const ref = useRef(null);
+    const ref = useRef<HTMLElement>(null);
 
     useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
+        const observer = new (window as any).IntersectionObserver(([entry]: IntersectionObserverEntry[]) => {
             setIntersecting(entry.isIntersecting);
         }, options);
 
         if (ref.current) {
-            observer.observer(ref.current);
+            observer.observe(ref.current);
         }
         return () => {
             if (ref.current) {
