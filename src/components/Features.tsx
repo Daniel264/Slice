c102135cf9473daf2a6a27fa97b37d9eb29f9ee8
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-26 00:40:05 +0100

    feat: add the feature images to the feature content and adjust the layout

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Badge } from "./Badge";
+import Image from "next/image";
 
 const Features = () => {
     return (
@@ -12,18 +13,27 @@ const Features = () => {
             </div>
             <div className="features_content">
                 <div>
+                    <div className="features_image">
+                        <Image src="/assets/images/features_image_one.svg" width={45} height={45} alt={"features-image"} />
+                    </div>
                     <h4>Improved stability</h4>
                     <p>
                         Historically, real estate has provided <br /> lower volatility compared to stock <br /> investments.
                     </p>
                 </div>
                 <div>
+                    <div className="features_image">
+                        <Image src={"/assets/images/features_image_two.svg"} width={45} height={45} alt={"features-image-two"} />
+                    </div>
                     <h4>Steady income</h4>
                     <p>
                         Generate a consistent income through <br /> renting without being correlated to the <br /> market trends.
                     </p>
                 </div>
                 <div>
+                    <div className="features_image">
+                        <Image src={"/assets/images/features_image_three.svg"} width={45} height={45} alt={"features-image-three"} />
+                    </div>
                     <h4>Inflation protection</h4>
                     <p>
                         Safeguard your investment against <br /> inflation with our built-in inflation <br /> protection toolset.
