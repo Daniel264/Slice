1d2718e39299cf2ae775f8383a5340491b3a551f
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-26 20:27:51 +0100

    refactor: change the layout of the savings page

--- src/components/Savings.tsx
+++ src/components/Savings.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import NumberCounter from "./NumberCounter";
 import { numbercounter } from "@/utilities/number-counter";
 import IntersectionObserver from "./IntersectionObserver";
+import { Badge } from "./Badge";
 
 const StyledSpan = styled.span`
     color: #737373;
@@ -11,13 +12,23 @@ const StyledSpan = styled.span`
     // padding-right: 10px;
 `;
 
+const AlignText = styled.div`
+    text-align: left;
+    width: 10%;
+    justify-content: start;
+    display: flex;
+`;
+
+const H2Text = styled.h2`
+    font-weight: 600;
+`;
+
 const SavingsWrapper = styled.section`
 
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     height: max-content;
-    align-items: center;
     padding: 2rem;
     margin-top: 5rem;
     border: 1px solid #888;
@@ -36,22 +47,27 @@ const Savings = () => {
     const [ref, isVisible] = IntersectionObserver({ threshold: 0 });
     return (
         <SavingsWrapper className="savings-wrapper">
+            <AlignText>
+                <div className="badge-div">
+                    <Badge>Savings</Badge>
+                </div>
+            </AlignText>
             <article>
-                <h2>
+                <H2Text>
                     <StyledSpan>Optimise your return rate</StyledSpan> and retain more of your income
-                </h2>
+                </H2Text>
 
                 <p>
                     Secure premium profits. We seamlessly provide you <br /> with the highest returns. Keep up to an extra <br /> 17.6% after taxes. some of your earnings could <br /> be exempt from local and city taxes.
                 </p>
             </article>
-{/* 
+            {/* 
             {isVisible && ( */}
-                <article ref={ref}>
-                    {numbercounter.map((item) => (
-                        <NumberCounter key={item.id} id={0} number={item.number} sign={item.sign} text={item.text} />
-                    ))}
-                </article>
+            <article ref={ref}>
+                {numbercounter.map((item) => (
+                    <NumberCounter key={item.id} id={0} number={item.number} sign={item.sign} text={item.text} />
+                ))}
+            </article>
             {/* )} */}
         </SavingsWrapper>
     );
