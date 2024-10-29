98c398d3c71f2e3d85f225c708418ddc48346d54
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-29 20:47:52 +0100

    feat: style the get-started section

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -4,13 +4,15 @@ import Image from "next/image";
 
 const GetStarted = () => {
     return (
-        <section>
-            <article>
-                <h1></h1>
+        <section className="getStarted_section">
+            <article className="getStarted_article">
+                <h1>
+                    Invest in real estate <br /> today and start build <br /> your wealth
+                </h1>
                 <Button buttonType="white_butn">Get Started</Button>
             </article>
-            <div>
-                <Image src="/assets/images/low-image.svg" alt="Image saying Get started" width={500} height={500} />
+            <div className="getStarted_image">
+                <Image src="/assets/images/low-image.svg" alt="Image saying Get started" width={600} height={500} />
             </div>
         </section>
     );
