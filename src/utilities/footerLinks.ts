59fc7a04b0b6f111d901091334cfaa95497a7003
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-29 21:33:17 +0100

    Create the footerlink array

---
+++ src/utilities/footerLinks.ts
@@ -0,0 +1,15 @@
+export const firstLink = [
+    { id: 1, text: "How does it work", url: "/" },
+    { id: 2, text: "Benefits", url: "/" },
+    { id: 3, text: "Investing Strategies", url: "/" },
+    { id: 4, text: "Pricing", url: "/" },
+    { id: 5, text: "Security", url: "/" },
+    { id: 6, text: "Careers", url: "/" },
+];
+export const secondLink = [
+    { id: 1, text: "FAQ", url: "/" },
+    { id: 2, text: "Learn Center", url: "/" },
+    { id: 3, text: "More Products", url: "/" },
+    { id: 4, text: "Help Center", url: "/" },
+    { id: 5, text: "Updates", url: "/" },
+];
